b15d8869f7aedf243277d186e932f7f88ca488d1
MCC-28's Mac Mini <mcc-28smacmini@MCC-28ss-Mac-mini.local>
2020-03-06 09:13:54 +0000

    prepare to deploy

--- helpers/bcrypt.js
+++ helpers/bcrypt.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcryptjs')
 
 module.exports = {
     hashPassword: (password) => {
+        console.log(+process.env.SALT)
+        console.log(password)
         var salt = bcrypt.genSaltSync(+process.env.SALT)
         return bcrypt.hashSync(password, salt)
     },
