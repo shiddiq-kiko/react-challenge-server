dd9ce1cf5ed26e0163e0f48ae2d40b772a859f3f
MCC-28's Mac Mini <mcc-28smacmini@MCC-28ss-Mac-mini.local>
2020-03-06 04:02:38 +0000

    prepare to deploy

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,48 +1,41 @@
 module.exports = (err, req, res, next) => {
-    const error = err
-    console.log(error)
-    if(error.name === 'SequelizeValidationError'){
-        const messages = []
-        for(let i = 0; i < error.errors.length; i++){
-            messages.push(error.errors[i].message)
-        }
-        res.status(400).json({
-            err : messages
-        })
+  const error = err;
+  console.log(error);
+  if (error.name === "SequelizeValidationError") {
+    const messages = [];
+    for (let i = 0; i < error.errors.length; i++) {
+      messages.push(error.errors[i].message);
     }
-    else if(error.name === 'FavoriteFailed'){
-        res.status(400).json({
-            err : error.message
-        })
-    }
-    else if(error.name === 'loginError'){
-        res.status(400).json({
-            err : 'email / password is wrong'
-        })
-    }
-    else if(error.name === 'dataNotFound'){
-        res.status(404).json({
-            err : 'Cannot find Data'
-        })
-    }
-    else if(error.name === 'NotAuthorized'){
-        res.status(401).json({
-            err : error.message
-        })
-    }
-    else if(error.name === 'SequelizeDatabaseError'){
-        res.status(500).json({
-            err : 'Internal Server Error'
-        })
-    }
-    else if(error.name === 'JsonWebTokenError'){
-        res.status(401).json({
-            err : 'You Must login first'
-        })
-    }
-    else{
-        res.status(500).json({
-            err : 'Internal Server Error'
-        })
-    }
-}
+    res.status(400).json({
+      err: messages
+    });
+  } else if (error.name === "FavoriteFailed") {
+    res.status(400).json({
+      err: error.message
+    });
+  } else if (error.name === "LoginError") {
+    res.status(400).json({
+      err: "email / password is wrong"
+    });
+  } else if (error.name === "dataNotFound") {
+    res.status(404).json({
+      err: "Cannot find Data"
+    });
+  } else if (error.name === "NotAuthorized") {
+    res.status(401).json({
+      err: error.message
+    });
+  } else if (error.name === "SequelizeDatabaseError") {
+    res.status(500).json({
+      err: "Internal Server Error"
+    });
+  } else if (error.name === "JsonWebTokenError") {
+    res.status(401).json({
+      err: "You Must login first"
+    });
+  } else {
+    res.status(500).json({
+      err: "Internal Server Error"
+    });
+  }
+};
